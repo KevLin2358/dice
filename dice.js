8598f6624844af99e4de8e92d4a76d0f49786d8d
KevLin2358 <klin2358@gmail.com>
2023-04-03 14:15:51 -0400

    added dice class

---
+++ dice.js
@@ -0,0 +1,26 @@
+class Dice {
+    constructor(numOfDice = 1) {
+        this.min = 1
+        this.max = 7
+        this.numOfDice = numOfDice
+    }
+
+    get roll() {
+        let res = []
+
+        for (let i = 0; i < this.numOfDice; i++) {
+            let dice = this.randomInt(this.min, this.max)
+            res.push(dice)    
+        }
+
+        return res
+    }
+
+    randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+}
+
+const dice = new Dice(3)
+
+console.log(dice.roll)
